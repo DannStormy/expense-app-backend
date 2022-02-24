4ab3935f04ccbab16d5f14e7f6ea017951a20d3f
dann <dillydot22@gmail.com>
2022-02-24 12:58:56 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const User = require('./models/user.model');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/expense-app');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(req.body)
+    try {
+        //save body contents in variable
+        await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: newPassword
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ err, error: 'Duplicate email' })
+    }
+})
+
+
+app.get('/login', async (req, res) => {
+    const user = await User.findOne({
+        username: req.body.username,
+    })
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (validPassword) {
+        const token = jwt.sign({
+            username: user.username
+        }, 'secret123')
+        return res.json({ status: 'ok', user: token })
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
+})
+
+app.listen(1337, () => {
+    console.log('Server started on 1337');
+})
---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true, unique: true},
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
+    },
+    { collection: 'user-data' }
+)
+
+const User = mongoose.model('UserData', userSchema);
+
+module.exports = User;
